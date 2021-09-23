b5e43d06efa0d0675a9c2b21298b65a2ef91c951
salas <salas@salas.local>
2021-09-23 17:19:53 -0500

    Angie commit

---
+++ Cafetal1/Registrar_peso_cafe.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Cafetal1
+{
+    partial class Registrar_peso_cafe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registrar_peso_cafe));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbBx_recolector = new System.Windows.Forms.ComboBox();
+            this.txtbx_peso_cafe = new System.Windows.Forms.TextBox();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.btn_registrar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            this.txtBx_pago_recolector = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Louis George Café", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(52)))), ((int)(((byte)(34)))));
+            this.label1.Location = new System.Drawing.Point(215, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecciona recolector";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Louis George Café", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(52)))), ((int)(((byte)(34)))));
+            this.label2.Location = new System.Drawing.Point(419, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "KG";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Louis George Café", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(52)))), ((int)(((byte)(34)))));
+            this.label3.Location = new System.Drawing.Point(215, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese peso del café";
+            // 
+            // cmbBx_recolector
+            // 
+            this.cmbBx_recolector.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(242)))));
+            this.cmbBx_recolector.Font = new System.Drawing.Font("Louis George Café", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbBx_recolector.FormattingEnabled = true;
+            this.cmbBx_recolector.Location = new System.Drawing.Point(220, 178);
+            this.cmbBx_recolector.Name = "cmbBx_recolector";
+            this.cmbBx_recolector.Size = new System.Drawing.Size(228, 33);
+            this.cmbBx_recolector.TabIndex = 3;
+            // 
+            // txtbx_peso_cafe
+            // 
+            this.txtbx_peso_cafe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(242)))));
+            this.txtbx_peso_cafe.Font = new System.Drawing.Font("Louis George Café", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_peso_cafe.Location = new System.Drawing.Point(220, 271);
+            this.txtbx_peso_cafe.Name = "txtbx_peso_cafe";
+            this.txtbx_peso_cafe.Size = new System.Drawing.Size(193, 33);
+            this.txtbx_peso_cafe.TabIndex = 4;
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(242)))));
+            this.btn_ingresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ingresar.Font = new System.Drawing.Font("Louis George Café", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ingresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(52)))), ((int)(((byte)(34)))));
+            this.btn_ingresar.Location = new System.Drawing.Point(488, 270);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(118, 40);
+            this.btn_ingresar.TabIndex = 5;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = false;
+            // 
+            // btn_registrar
+            // 
+            this.btn_registrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(242)))));
+            this.btn_registrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_registrar.Font = new System.Drawing.Font("Louis George Café", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_registrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(52)))), ((int)(((byte)(34)))));
+            this.btn_registrar.Location = new System.Drawing.Point(220, 407);
+            this.btn_registrar.Name = "btn_registrar";
+            this.btn_registrar.Size = new System.Drawing.Size(193, 40);
+            this.btn_registrar.TabIndex = 6;
+            this.btn_registrar.Text = "Registrar";
+            this.btn_registrar.UseVisualStyleBackColor = false;
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(242)))));
+            this.btn_cancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_cancelar.Font = new System.Drawing.Font("Louis George Café", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(52)))), ((int)(((byte)(34)))));
+            this.btn_cancelar.Location = new System.Drawing.Point(438, 407);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(168, 40);
+            this.btn_cancelar.TabIndex = 7;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = false;
+            // 
+            // txtBx_pago_recolector
+            // 
+            this.txtBx_pago_recolector.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(242)))));
+            this.txtBx_pago_recolector.Font = new System.Drawing.Font("Louis George Café", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBx_pago_recolector.Location = new System.Drawing.Point(220, 341);
+            this.txtBx_pago_recolector.Name = "txtBx_pago_recolector";
+            this.txtBx_pago_recolector.Size = new System.Drawing.Size(386, 33);
+            this.txtBx_pago_recolector.TabIndex = 8;
+            // 
+            // Registrar_peso_cafe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(214)))), ((int)(((byte)(171)))));
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.txtBx_pago_recolector);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_registrar);
+            this.Controls.Add(this.btn_ingresar);
+            this.Controls.Add(this.txtbx_peso_cafe);
+            this.Controls.Add(this.cmbBx_recolector);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Registrar_peso_cafe";
+            this.Text = "Registrar_peso_cafe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbBx_recolector;
+        private System.Windows.Forms.TextBox txtbx_peso_cafe;
+        private System.Windows.Forms.Button btn_ingresar;
+        private System.Windows.Forms.Button btn_registrar;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.TextBox txtBx_pago_recolector;
+    }
+}
